48c8ee9b603ce3a780082e3134f665af5dce3fcf
danielmotofelea <juanmotofelea@gmail.com>
2016-12-06 17:16:21 +0100

    Add files via upload

--- Jugador.java
+++ Jugador.java
@@ -8,10 +8,11 @@ public class Jugador {
         int []identificacion = new int[3];              // Posicion 0: nºgeneracion // Posicion 1: nº mesa // Posicion 3: nº jugador
         Carta []cartasEnMano ;                          // Las dos cartas en mano que en principio los tomamos como strings
         Carta []cartasComunes ;                         // Las 5 cartas comunes de la mesa para ver nuestra mejor mano
+        ArrayList<Carta> mejorMano;                     // La mejor combinacion de cartas sobre la mesa
     }
     public Jugador(int fichas, int fichasGanadas, int fichasApostadas, int manosGanadas,
                    int manosJugadas, float fitness, int [] identificacion, String []cartasEnMano,
-                   String []cartasComunes){
+                   String []cartasComunes, ArrayList<Carta> mejorMano){
 
         fichas = fichas;
         fichasGanadas = fichasGanadas;
@@ -25,36 +26,31 @@ public class Jugador {
             cartasEnMano[j] = new Carta();
         for(int k=0; k<5; k++)
             cartasComunes[k] = new Carta();
+        mejorMano = new ArrayList<Carta>();
     }
     public float calcularFitness(int fichasGanadas, int fichasApostadas, int manosGanadas, int manosJugadas, float fitness){
-        // Como se calcula al final de cada partida tengo que ver como da por finalizada la partida Florin
-        if(manosGanadas != 0 || fichasGanadas != 0)
-            fitness = fitness + calcularFitnessRecursivo();
+        // Como se calcula al final de cada mano tengo que ver como da por finalizada la partida Florin
+        if((manosJugadas != 0) && (fichasApostadas != 0))
+            fitness = ((manosGanadas/manosJugadas)*(fichasGanadas/fichasApostadas))/100;
         else
             fitness = 0;
-
         return fitness;
     }
-    public float calcularFitnessRecursivo(){
-        float fitnessProvisional;
 
-
-        return fitnessProvisional;
-    }
-    public String[] enseñarCartas(String []cartasEnMano, String []cartasComunes){
-        String [] mejorManoEnseñarCartas = new String[5];
-        for(int i=0; i<5; i++)
-            mejorManoEnseñarCartas[i] =  verMejorMano(cartasEnMano,cartasComunes).mejorMano[i];
-        return mejorManoEnseñarCartas;
+    public ArrayList<Carta> enseñarCartas(ArrayList<Carta> mejorMano){
+        return mejorMano;
     }
 
-    public ArrayList<Carta>[] verMejorMano(String [] cartasEnMano, String [] cartasComunes){
+    public ArrayList<Carta>[] verMejorMano(Carta [] cartasEnMano, Carta [] cartasComunes){
         // Suponiendo que Florin lo haya implementado asi
-        ArrayList<Carta>[] mejorMano;
-        ArrayList<Carta>[] manoProvisional;
-        manoProvisional = // array creado para añadir las 7 cartas al borroso y que devuelva la mejor mano
-        for(int i=0; i<5; i++)
-            mejorMano[i] = new Carta();
+
+        ArrayList<Carta> manoProvisional = new ArrayList<Carta>();    // array creado para añadir las 7 cartas al borroso y que devuelva la mejor mano
+        for(int i=0; i<2; i++)
+            manoProvisional.add(cartasEnMano[i]);        // se añaden las cartas que se tenga actualmente en un arraylist unificado
+        for(int j=0; j<5; j++)                          // para pasarselo a otro metodo y que evalue la mejor mano
+            manoProvisional.add(cartasComunes[j]);
+        // Ahora habria que hacer una llamada a la logica borrosa o a las reglas para devolver la mejor mano.
+        //mejorMano = calcularMejorMano(manoProvisional);
         return mejorMano;
     }
     public void tomarDecision(){ // Tomare cada decision como un entero, 0 no ir, 1 pasar, 2 apostar
