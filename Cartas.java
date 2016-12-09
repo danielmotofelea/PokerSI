2fae3b69b1356460530e2f25eb7134d9d5efd5a6
KarkTatum <karktatum@gmail.com>
2016-12-09 19:30:56 +0100

    Create Cartas.java

---
+++ Cartas.java
@@ -0,0 +1,32 @@
+package Version1;
+
+/**
+ * Este es el objeto carta, tiene dos campos tipo string que se corresponden al valor y al palo
+ */
+
+public class Cartas {
+    private String Valor="";
+    private String Palo="";
+
+    /**
+     *Constructor de la carta, le pasamos el valor y el palo.
+     */
+    public Cartas(String Valor,String Palo){
+        this.Palo=Palo;
+        this.Valor=Valor;
+    }
+
+    /**
+     *Metodo toString para comprobar el correcto funcionamiento
+     */
+    public String toString() {
+        return "Cartas{" +
+                "Valor='" + Valor + '\'' +
+                ", Palo='" + Palo + '\'' +
+                '}';
+    }
+
+    /**
+     * Ire aniadiendo mas metodos segun se vayan necesitando asi como los basicos set and get.
+     */
+}
