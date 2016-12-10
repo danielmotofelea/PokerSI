f957637e8de757709deefb0e448d6e01c8c65c26
juanmanuelvc <juanma.vicenteca@gmail.com>
2016-12-10 20:17:15 +0100

    Cambiado el nombre de la clase

--- Carta.java
+++ Carta.java
@@ -1,36 +1,43 @@
-package Version1;
+package pokerSI;
 
 /**
  * Este es el objeto carta, tiene dos campos tipo string que se corresponden al valor y al palo
  */
 
-public class Cartas {
-    private String Valor;
-    private String Palo;
+public class Carta {
+    private String valor;
+    private String palo;
 
     // Constructor sin parametros para facilitar las inicializaciones en la clase Jugador
-    public Cartas(){
-        this.Palo = "";
-        this.Valor = "";
+    public Carta(){
+        this.palo = "";
+        this.valor = "";
     }
     /**
      *Constructor de la carta, le pasamos el valor y el palo.
      */
-    public Cartas(String Valor,String Palo){
-        this.Palo=Palo;
-        this.Valor=Valor;
+    public Carta(String valor,String palo){
+        this.palo=palo;
+        this.valor=valor;
     }
 
     /**
      *Metodo toString para comprobar el correcto funcionamiento
      */
     public String toString() {
-        return "Cartas{" +
-                "Valor='" + Valor + '\'' +
-                ", Palo='" + Palo + '\'' +
+        return "Carta{" +
+                "Valor='" + valor + '\'' +
+                ", Palo='" + palo + '\'' +
                 '}';
     }
 
+    public String getValor() {
+        return valor;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
     /**
      * Ire aniadiendo mas metodos segun se vayan necesitando asi como los basicos set and get.
      */
