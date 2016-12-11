57815d3085f0d682e6424a0da89c768876993b79
Javi Fdez <el-cyd@hotmail.com>
2016-12-11 21:02:21 +0100

    Primera aproximacion clase Torneo

--- Torneo.java
+++ Torneo.java
@@ -6,33 +6,47 @@ package PokerSI;
  * Created by Javi on 09/12/2016.
  */
 public class Torneo {
-    protected Jugador[] finalistas;
+    protected ArrayList<Jugador> finalistas;
     private ArrayList<Jugador> participantes;
+    private int idGen;
 
-    public Torneo(ArrayList<Jugador> participantes) {
-        finalistas= new Jugador[8];
+    /**
+     *
+     * @param participantes ArrayList que contiene a todos los participantes de un torneo.
+     * @param idGen identificador de la generación actual de los jugadores.
+     *
+     * El constructor solamente instancia un array de 8 finalistas y se guarda los valores que se le pasa por parámetro.
+     */
+    public Torneo(ArrayList<Jugador> participantes, int idGen) {
+        finalistas =new ArrayList<Jugador>();
+        participantes=new ArrayList<Jugador>();
         this.participantes.addAll(participantes);
+        this.idGen=idGen;
     }
 
-
-    public void colocarJugadores(){
-        for(int i=7;i>=0;i--) {
-            /**
-             * Por cada  mesa habrá que:
-             * 1. Como las primeras 8 posiciones de "participantes" son los participantes de la mesa final de la generación anterior, cada uno irá a una mesa diferente(posicion 0 de "participantes")
-             * 2. A partir de la posicion i, se incluirán 7 jugadores más a cada mesa
-             * 3. Al incluir 7 jugadores más en la mesa, se iniciará la partida hasta que termine y la mesa devuelva al ganador. Ese ganador se incluirá en "finalistas"
-             * 4. Se avanza a la siguiente mesa y se repite la operación
-             */
+    /**
+     * El método realizarTorneo se encarga de colocar en las mesas a los jugadores correspondientes, iniciar la partida y guardar a los ganadores.
+     * Para ello se crea un ArrayList de jugadores que, por cada mesa, añade a un finalista de la generación anterior y a 7 jugadores que han sido generados en esta generación.
+     * Luego crea un objeto mesa para que esos jugadores jueguen la partida.
+     * El ganador de la mesa se guarda en el array de finalistas.
+     * Esto se repite 8 veces en total
+     * Finalmente se jugaría la mesa final para actualizar los fitness
+     */
+    public void realizarTorneo() {
+        Mesa m;
+        ArrayList<Jugador> jugadoresMesa = new ArrayList<Jugador>();
+        for (int i = 0; i < 8; i++) {
+            int posOtrosJug=8;
+            jugadoresMesa.add(participantes.get(i));//Los 8 primeros jugadores del ArrayList son los finalistas de la generación anterior.
+            for(int j=0;j<7;j++) {
+                jugadoresMesa.add(participantes.get(posOtrosJug));
+                posOtrosJug++;
+            }
+            m= new Mesa(jugadoresMesa, i, idGen);//Constructor mesa: Mesa(Jugadores, idMesa, idGen);
+            finalistas.add(m.jugar());//El método jugar devuelve el ganador de la mesa.
         }
-
-            /**
-             * 5. Al terminar las 8 mesas, se colocarán a los 8 finalistas en una nueva mesa y se realizará una última partida.
-             * (Se recalculan los fitness aunque todos ellos van a salvarse para la próxima generación)
-             *
-             *
-             *
-             *
-            * */
+        m=new Mesa(finalistas,idGen);//Constructor mesa: Mesa(Jugadores, idGen);
+        m.jugarMesaFinal();
     }
+
 }
