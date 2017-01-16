8f33b0dd5316e3193ac4b85be36d22ced8c1af25
KarKCyd <el-cyd@hotmail.com>
2017-01-16 19:33:19 +0100

    Actualizacion de clase Torneo

--- Torneo.java
+++ Torneo.java
@@ -27,7 +27,7 @@ public class Torneo {
 
     /**
      * El método realizarTorneo se encarga de colocar en las mesas a los jugadores correspondientes, iniciar la partida y guardar a los ganadores.
-     * Para ello se crea un ArrayList de jugadores que, por cada mesa, añade a un finalista de la generación anterior y a 7 jugadores que han sido generados en esta generación.
+     * Para ello se crea un ArrayList de jugadores que, por cada mesa, 8 jugadores que han sido generados en esta generación.
      * Luego crea un objeto mesa para que esos jugadores jueguen la partida.
      * El ganador de la mesa se guarda en el array de finalistas.
      * Esto se repite 8 veces en total
@@ -35,36 +35,28 @@ public class Torneo {
      */
     public ArrayList<Jugador> realizarTorneo() {
         Mesa m;
-        //ArrayList<Jugador> jugadoresMesa = new ArrayList<Jugador>();
-        int posOtrosJug=8; //Guardara la posicion donde empezara a coger los jugadores no finalistas para la siguiente mesa
+        int njugador=0;
 
-        for (int i = 0; i < 8; i++) {
+        for(int j=0;j<8/*Numero de mesas*/;j++){
             ArrayList<Jugador> jugadoresMesa = new ArrayList<Jugador>();
+	        for (int i = 0; i < 8/*Numero de jugadores por mesa*/; i++) {
+	            jugadoresMesa.add(participantes.get(njugador++));
+			}
 
-            jugadoresMesa.add(participantes.get(i));//Los 8 primeros jugadores del ArrayList son los finalistas de la generación anterior.
-
-
-            for(int j=0;j<7;j++){
-                jugadoresMesa.add(participantes.get(posOtrosJug++));
-            }
-
-                m = new Mesa(jugadoresMesa, (i + 1), idGen);//Constructor mesa: Mesa(Jugadores, idMesa, idGen); El id de la mesa va de 1 a 8
-                finalistas.add(m.jugar());//El método jugar devuelve el ganador de la mesa.
-
-        }
+	        m = new Mesa(jugadoresMesa, (j + 1), idGen);//Constructor mesa: Mesa(Jugadores, idMesa, idGen); El id de la mesa va de 1 a 8
+	        finalistas.add(m.jugar());//El método jugar devuelve el ganador de la mesa.
+    	}
         
         for(int i=0; i<finalistas.size();i++){
-            finalistas.get(i).calcularFitnessMesa();
+            finalistas.get(i).calcularFitnessMesa();//Calculo el fitness de los ganadores de las mesas clasificatorias.
         }
         m=new Mesa(finalistas,idGen);//Constructor mesa: Mesa(Jugadores, idGen);
         for(int i=0; i<finalistas.size();i++){
-            finalistas.get(i).gradoDeSoporte(i,idGen);
+            finalistas.get(i).gradoDeSoporte(i,idGen);//Escribo en fichero los grados de soporte de las reglas del borroso de los finalistas de la generacion
         }
-        m.jugar();
-        //System.out.println("MESA DE LOS FINALISTAS");
-        //m.MuestraContenido();
-        return finalistas;
+        m.jugar();//Juega la mesa final
 
+        return finalistas;
 
     }
 
